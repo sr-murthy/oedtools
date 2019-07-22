27ea7a9d6b34b176a62ea3fae47751fa6dad7588
Sandeep Murthy <smurthy@protonmail.ch>
2019-07-22 21:46:06 +0100

    Set version `0.1.0` in package init

--- oedtools/__init__.py
+++ oedtools/__init__.py
@@ -1,6 +1,1 @@
-import io
-import os
-
-from .schema import SCHEMA_DIR
-with io.open(os.path.join(SCHEMA_DIR, 'schema_version.txt'), 'r', encoding='utf-8') as f:
-    __version__ = f.readline().strip()
+__version__ = '0.1.0'
